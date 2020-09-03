9ad6fbb8597f74a5adf4335ae23b4e9a83b3bdb1
Ananda Rauf <uzumaki.nanda@rocketmail.com>
2020-09-04 01:12:25 +0700

    Update and rename SketchwareUtil.java to BatriUtil.java

--- app/src/main/java/com/batrisyia/indo/BatriUtil.java
+++ app/src/main/java/com/batrisyia/indo/BatriUtil.java
@@ -12,7 +12,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Random;
 
-public class SketchwareUtil {
+public class BatriUtil {
 public static void showMessage(Context _context, String _s) {
 Toast.makeText(_context, _s, Toast.LENGTH_SHORT).show();
 }
